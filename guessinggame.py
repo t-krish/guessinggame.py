6b589d48bb5cb9281cb591ffeb2fd9532b36156f
t-krish <95111595+t-krish@users.noreply.github.com>
2021-11-26 20:57:26 -0500

    Add files via upload

---
+++ guessinggame.py
@@ -0,0 +1,16 @@
+import random
+gno=random.randint(1, 100)
+guessed = False
+while guessed == False:
+    guess = input('What number do you think it is?: ')
+    guess = int(guess)
+    if guess > gno:
+        print('Try a little lower')
+    if guess < gno:
+        print('Try a little higher')
+    if guess == gno:
+        print('CORRECT!!!')
+        guessed = True
+
+
+#Completed Project
